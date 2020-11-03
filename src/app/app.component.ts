8a6bbc2f1921e2f5b10fb82358b9bb7619acd818
Sheetal575 <sheetaldadh9@gmail.com>
2020-11-03 09:51:07 +0530

    angular forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, HostBinding } from '@angular/core';
+import { NgForm } from '@angular/forms';
+// import {FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Theme-Change';
+  isDarkTheme =  true;
+  firstName = '';
+  lastName = '';
+  emailAddress = '';
+  ngOnInit(): void {
+    this.isDarkTheme = localStorage.getItem('theme') === 'dark' ? true : false;
+  }
+  // tslint:disable-next-line: typedef
+  storeThemeSelection(){
+    localStorage.setItem('theme', this.isDarkTheme ? 'dark' : 'Light');
+  }
+
+  // tslint:disable-next-line: typedef
+  onSubmit(form: NgForm){
+    console.log(form);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
